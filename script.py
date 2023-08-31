c077d6a56c30ae7881d2c2f40f703c96593a8f91
khushalcrest <133368645+khushalcrest@users.noreply.github.com>
2023-08-31 16:45:34 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,35 @@
+import json
+import os
+
+
+ukeys = set()
+ll = []
+d = {}
+dd = {}
+
+directory = os.path.join("c:\\","Users\\harshit.patel\\Desktop\\Comcast\\github-enterprise")
+for root,dirs,files in os.walk(directory):
+    for file in files:
+       if file.endswith(".json"):
+            f=open(file, 'r')
+            for x in f:
+                y = json.loads(x)
+
+                for k, v in y.items():
+                    dd[k] = v
+
+                d.setdefault(y["action"], list()).append(str(y))
+                
+            nd = {}
+            for k, v in d.items():
+                nd[k] = list(v)
+
+
+            # for k, v in dd.items():
+            #     print(k + ", " + str(v))
+
+
+            f.close()
+
+for k, v in sorted(d.items()):
+	print(k + "#" + str(v[0]))
